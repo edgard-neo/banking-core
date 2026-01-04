98b6fbcbbf729a0d6a7dd5ae15e1d0b491a5d2b1
Edgar Mendes <mendesbr92@gmail.com>
2026-01-04 15:10:13 -0300

    feat: criado os metodos para salvar, buscar todos e adicionado o contador para o id

---
+++ src/main/java/com/br/repository/ContaBancariaRepository.java
@@ -0,0 +1,16 @@
+package com.br.repository;
+
+import java.util.Set;
+import com.br.domain.ContaBancaria;
+
+public interface ContaBancariaRepository {
+
+    void adicionarUsuario(ContaBancaria usuario);
+
+    ContaBancaria buscarUsuarioPelaID(Long id);
+
+    Set<ContaBancaria> buscarTodosUsuarios();
+
+    // TODO: Obter histórico completo da conta (ordenado por data, decrescente,
+    // somente leitura)
+}
---
+++ src/main/java/com/br/repository/ContaBancariaRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.br.repository;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+import com.br.domain.ContaBancaria;
+
+public class ContaBancariaRepositoryImpl implements ContaBancariaRepository {
+
+    private Set<ContaBancaria> bancoDeDadosInMemoria = new LinkedHashSet<>();
+
+    private AtomicLong contador = new AtomicLong(0);
+
+
+    @Override
+    public void adicionarUsuario(ContaBancaria usuario) {
+
+        // adiciona o ID no o objeto antes de salvar
+        usuario.setIDapenasPeloRepository(contador.incrementAndGet());
+        bancoDeDadosInMemoria.add(usuario);
+    }
+
+    @Override
+    public ContaBancaria buscarUsuarioPelaID(Long id) {
+
+        return bancoDeDadosInMemoria.stream().filter(b -> b.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public Set<ContaBancaria> buscarTodosUsuarios() {
+        return bancoDeDadosInMemoria;
+    }
+}
